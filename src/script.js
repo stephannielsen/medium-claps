81f7ae228024e115888492127ef63d7ae1e4152e
Stephan Nielsen <stephannielsen@gmail.com>
2018-10-15 14:13:58 +0200

    Parsing urls correctly, pointing to medium.com.

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 (function() {
     "use strict"; // Start of use strict
 
+    const mediumUrl = "https://medium.com"
 
     $.getJSON('http://allorigins.me/get?url=https://medium.com/@stephannielsen/has-recommended&callback=?', function(data) {
         console.log($.parseHTML(data.contents));
@@ -8,26 +9,30 @@
         var section = $(root).find("section")[0];
         var container = $(section).children()[1];
         var posts = $(container).children().first().children().toArray();
-        posts.shift(); //hei we got all the posts on the page
+        posts.shift();
 
         posts.forEach(p => {
             //each post is wrapped in a div, so traverse one down first
             p = $(p).find("div").first()[0];
             var tmp = $(p).children().toArray();
-            var authorInfo = tmp[0];
-            var postInfo = tmp[1];
+            var authorInfo = $(tmp[0]);
+            var postInfo = $(tmp[1]);
             var post = {
-                authorImage: $(authorInfo).find("img")[0].src,
-                authorLink: $(authorInfo).find("a")[0].href,
-                authorName: $(authorInfo).find("span").first().find("a")[0].innerHTML,
-                postImage: $(postInfo).find("img")[0].src,
-                postLink: $(postInfo)[0].href,
-                postTitle: $(postInfo).find("h1")[0].innerHTML
+                authorImage: authorInfo.find("img")[0].src,
+                authorLink: mediumUrl + substringByString(authorInfo.find("a")[0].href, "/@", "?"),
+                authorName: authorInfo.find("span").first().find("a")[0].innerHTML,
+                postImage: postInfo.find("img")[0].src,
+                postLink: mediumUrl + substringByString(postInfo[0].href, "/p/", "?"),
+                postTitle: postInfo.find("h1")[0].innerHTML
             }
             var stats = tmp[2];
             console.log(post);
         });
     });
 
-    //div#root > div > section > 2nd div > div > 2nd div
+    var substringByString = (full, begin, end) => {
+        var startIndex = full.indexOf(begin);
+        var endIndex = full.indexOf(end);
+        return full.substring(startIndex, endIndex);
+    }
 })();
